381f09863a4217192b30b255c4e2a2a59db94fe8
Vasilkov Alexey <coyc@inbox.ru>
2017-10-05 00:16:48 +0300

    Stash added

--- app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
+++ app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
@@ -2,11 +2,13 @@ package com.github.coyclab.gittestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Main Activity";
     TextView mTextView;
 
     @Override
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.textView1);
         mTextView.setText(R.string.good_day);
+        Log.d(TAG, "onCreate Method is called");
         // Show toast message
         toastShow();
     }
