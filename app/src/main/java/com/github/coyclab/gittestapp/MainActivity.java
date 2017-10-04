59a9a4421201701871818fdb1e2a5fd4f47b6f8b
Vasilkov Alexey <coyc@inbox.ru>
2017-10-04 20:48:38 +0300

    finish work with feature/makeToast

--- app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
+++ app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.github.coyclab.gittestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toastShow();
+    }
+
+    public void toastShow() {
+        final Toast toast = Toast.makeText(getApplicationContext(), "Hello EPAM!", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
