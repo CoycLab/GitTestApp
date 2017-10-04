34d8bbeb0b630a50a10de99cbf494191a8db7b4f
Vasilkov Alexey <coyc@inbox.ru>
2017-10-04 21:12:38 +0300

    Some work in master-branch

--- app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
+++ app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
@@ -2,12 +2,17 @@ package com.github.coyclab.gittestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView1);
+        mTextView.setText(R.string.hello_everybody);
     }
 }
