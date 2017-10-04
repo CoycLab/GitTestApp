82aab8f0b48bc617038f3950cd0a1c74734df6d7
Vasilkov Alexey <coyc@inbox.ru>
2017-10-04 20:55:16 +0300

    finish work with feature/changeText

--- app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
+++ app/src/main/java/com/github/coyclab/gittestapp/MainActivity.java
@@ -2,13 +2,18 @@ package com.github.coyclab.gittestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView1);
+        mTextView.setText(R.string.good_day);
     }
 
     // futureRelease implementation
